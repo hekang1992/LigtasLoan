c1267ebe9ccd2df3cadc1471a9c6aa34af2994e5
何康 <hekang19921111@hotmail.com>
2024-10-12 14:53:10 +0800

    first commit

---
+++ LuckyLoan/Confing/LLLaunchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LLLaunchViewController.swift
+//  LuckyLoan
+//
+//  Created by 何康 on 2024/10/12.
+//
+
+import UIKit
+
+class LLLaunchViewController: LLBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ LuckyLoan/Main/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LuckyLoan
+//
+//  Created by 何康 on 2024/10/12.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    var ff = LLLaunchViewController()
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow.init()
+        window?.frame = UIScreen.main.bounds
+        window?.rootViewController = ff
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
